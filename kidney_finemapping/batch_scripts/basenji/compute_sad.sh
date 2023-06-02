49ae32ab568db1b7286287754e47bb04734d1335
Richard <richardshuai98@gmail.com>
2023-06-02 10:50:00 -0700

    preprocess script

--- kidney_finemapping/batch_scripts/basenji/compute_sad.sh
+++ kidney_finemapping/batch_scripts/basenji/compute_sad.sh
@@ -47,7 +47,7 @@ CHROM=chrXY
 kidney_finemapping/basenji/compute_sad.py \
   /clusterfs/nilah/richard/kidney_data/models/params_sc_kidney_regression.json \
   /clusterfs/nilah/pooja/kidney_data/train/regression_chr_models/train_bigwigs_${CHROM}/model_best.h5 \
-  /clusterfs/nilah/richard/kidney_data/220620_variants/susie/data/processed/snps_by_chrom/${CHROM}_snps.vcf \
+  /clusterfs/nilah/richard/refactor/220620_variants/susie/data/preprocessed/snps_by_chrom/${CHROM}_snps.vcf \
   -f /clusterfs/nilah/richard/genomes/hg38.ml.fa \
   --rc \
   --shifts "1,0,-1" \
